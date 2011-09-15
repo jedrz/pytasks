dff28875b978e35ed5106f0e07b008bc3ddb2e0c
Łukasz Jędrzejewski <lukasz892@gmail.com>
2011-09-15 09:10:37 +0200

    Added new options in the cli client (not showing numbers and sorting tasks)

--- src/cli.py
+++ src/cli.py
@@ -3,6 +3,7 @@
 
 
 import argparse
+import operator
 
 import parser
 import cons
@@ -39,29 +40,36 @@ class TaskListCLI(parser.TaskParser):
     def __str__(self):
         return self.list_tasks()
 
-    def list_tasks(self, comp=True, incomp=True, status=True):
+    def list_tasks(self, comp=True, incomp=True, status=True, number=True,
+                   sort=False):
         """Return a formatted string.
 
         arguments:
         comp -- show completed tasks
         incomp -- show incompleted tasks
         status -- show status of tasks
+        number -- show numbered tasks
 
-        Default the method returns formatted string with all tasks
+        Default the method returns formatted string with numbered all tasks
         and showed status.
         """
-        def filter_task(done):
+        def filter_task(date, done):
+            passed = False
             if comp and incomp:
-                return True
+                passed = True
             elif comp and done:
-                return True
+                passed = True
             elif incomp and not done:
-                return True
+                passed = True
             else:
-                return False
+                passed = False
+            if passed and sort and not date:
+                passed = False
+            return passed
         tasks = [task for task in self.get_tasks() \
-                if filter_task(task['done'])]
-
+                if filter_task(task['date'], task['done'])]
+        if sort:
+            tasks = sorted(tasks, key=operator.itemgetter('date'))
         def format_task(text, date, done):
             if date:
                 date = date.strftime(cons.DATE_FORMAT)
@@ -81,30 +89,41 @@ class TaskListCLI(parser.TaskParser):
                         return '[ ] {}'.format(text)
                 else:
                     return text
-        output = '\n'.join('{}. {}'.format(n + 1, format_task(task['text'], \
-                task['date'], task['done'])) for n, task in enumerate(tasks))
-        return output
+        if number:
+            return '\n'.join('{}. {}'.format(n + 1, format_task(
+                task['text'],
+                task['date'],
+                task['done']
+                )) for n, task in enumerate(tasks))
+        else:
+            return '\n'.join(format_task(
+                task['text'],
+                task['date'],
+                task['done']
+                ) for task in tasks)
 
 
 def main():
     ap = argparse.ArgumentParser(
             description='A todo list with interval option')
-    group = ap.add_mutually_exclusive_group()
-    group.add_argument('-a', '--add', nargs='+', metavar='DESCRIPTION',
-                       help='add a task')
-    group.add_argument('-d', '--delete', nargs=1, type=int, metavar='ID',
-                       help='delete a task')
-    group.add_argument('-m', '--mark', nargs=1, type=int, metavar='ID',
-                       help='mark a task')
-    group2 = ap.add_mutually_exclusive_group()
-    group2.add_argument('-l', '--list', action='store_true',
-                        help='list all tasks')
-    group2.add_argument('-c', '--comp', action='store_true',
-                        help='list completed tasks')
-    group2.add_argument('-i', '--incomp', action='store_true',
-                        help='list incompleted tasks')
+    ap.add_argument('-a', '--add', nargs='+', metavar='DESCRIPTION',
+                    help='add a task')
+    ap.add_argument('-d', '--delete', nargs=1, type=int, metavar='ID',
+                    help='delete a task')
+    ap.add_argument('-m', '--mark', nargs=1, type=int, metavar='ID',
+                    help='mark a task')
+    ap.add_argument('-l', '--list', action='store_true',
+                    help='list all tasks')
+    ap.add_argument('-c', '--comp', action='store_true',
+                    help='list completed tasks')
+    ap.add_argument('-i', '--incomp', action='store_true',
+                    help='list incompleted tasks')
+    ap.add_argument('--sorted', action='store_true',
+                    help='tasks with date sorted by it')
     ap.add_argument('-s', '--status', action='store_false',
                     help='don\'t show status')
+    ap.add_argument('-n', '--number', action='store_false',
+                    help='tasks not numbered')
     ap.add_argument('--update', action='store_true', help='update all tasks')
 
     args = ap.parse_args()
@@ -115,12 +134,15 @@ def main():
         tl.delete(args.delete[0])
     elif args.mark:
         tl.mark(args.mark[0])
-    if args.list:
-        print(tl.list_tasks(status=args.status))
+    elif args.list:
+        print(tl.list_tasks(status=args.status, number=args.number,
+                            sort=args.sorted))
     elif args.comp:
-        print(tl.list_tasks(incomp=False, status=args.status))
+        print(tl.list_tasks(incomp=False, status=args.status,
+                            number=args.number, sort=args.sorted))
     elif args.incomp:
-        print(tl.list_tasks(comp=False, status=args.status))
+        print(tl.list_tasks(comp=False, status=args.status,
+                            number=args.number, sort=args.sorted))
 
 
 if __name__ == '__main__':
